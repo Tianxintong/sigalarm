1f5708b3817a6973e619fc996a7efb3e06c9c926
txt <648436683@qq.com>
2017-05-13 17:52:43 +0800

    sequence list sigalarm

---
+++ utili1.h
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<stdio.h>
+#include<errno.h>
+#include<unistd.h>
+#include<string.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<sys/epoll.h>
+#include<pthread.h>
+
+#include<libgen.h>
