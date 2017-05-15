bac746773b1181ae8aa93e1e7abe5076a747daa4
txt <648436683@qq.com>
2017-05-15 20:16:24 +0800

    time_heap code

---
+++ time_heap_sigalarm/utili.h
@@ -0,0 +1,24 @@
+#ifndef _UTILI_H
+#define _UTILI_H
+//#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<stdio.h>
+#include<errno.h>
+#include<unistd.h>
+#include<string.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<sys/epoll.h>
+#include<pthread.h>
+#include<iostream>
+#include<libgen.h>
+ #include <signal.h>
+using namespace std;
+
+
+#endif
